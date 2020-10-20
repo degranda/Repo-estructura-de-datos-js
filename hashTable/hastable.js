c431cb741c492d2303a5c40f360793f2fae88035
Diego De Granda <degranda@gmail.com>
2020-10-20 13:36:05 -0500

    clase 10 metodo de get

--- hashTable/hastable.js
+++ hashTable/hastable.js
@@ -17,6 +17,18 @@ class HashTable {
     this.data[address].push([key, value]);
     return this.data;
   }
+  get(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
