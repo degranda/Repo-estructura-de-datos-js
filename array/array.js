984c3fddfecf9dab44fafd136062e30a632c9070
Diego De Granda <degranda@gmail.com>
2020-10-20 11:43:42 -0500

    first commit

---
+++ array/array.js
@@ -0,0 +1,18 @@
+// const array = ["Diego", "Karen", "Oscar"];
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+}
+
+const myArray = new MyArray();
