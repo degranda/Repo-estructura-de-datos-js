986f6bd686b2dd9f04c5008fe1ed4eadb4d59d63
Diego De Granda <degranda@gmail.com>
2020-10-20 12:07:45 -0500

    commit clase 6, metodos para eliminar elementos

--- array/array.js
+++ array/array.js
@@ -13,6 +13,25 @@ class MyArray {
     this.length++;
     return this.data;
   }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 const myArray = new MyArray();
