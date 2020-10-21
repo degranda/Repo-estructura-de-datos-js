ec3b45cc0cc9af64e0a715cd8e744accf6f3ecf4
Diego De Granda <degranda@gmail.com>
2020-10-21 09:48:57 -0500

    Agregando nodos a una SinglyLinkedList

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,20 +1,20 @@
-1-- > 2-- > 3-- > 4-- > 5-- > null;
-
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-};
+// 0 --> 1 -- > 2 -- > 3-- > 4-- > 5 -- > 6 --> null
+0;
+// let singlyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null,
+//         },
+//       },
+//     },
+//   },
+// };
 
 class Node {
   constructor(value) {
@@ -32,6 +32,25 @@ class MySinglyLinkedList {
     this.tail = this.head;
 
     this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+
+    return this;
   }
 }
 
