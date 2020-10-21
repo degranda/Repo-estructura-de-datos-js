9e54b28af887f86e14e43748e0bb5c6c9dc91b39
Diego De Granda <degranda@gmail.com>
2020-10-21 12:13:31 -0500

    trabajando un stack

---
+++ stacks/stack.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+
+    this.length++;
+
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.next;
+    this.length--;
+
+    return this;
+  }
+}
+
+const myStack = new Stack();
