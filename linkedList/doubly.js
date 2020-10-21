105facc135a8d7979ddbada0bc3d89c50c92f56a
Diego De Granda <degranda@gmail.com>
2020-10-21 10:34:09 -0500

    trabajando una doublyLinkedList

---
+++ linkedList/doubly.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class MyDoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    this.tail = this.head;
+
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    this.length++;
+
+    return this;
+  }
+}
+
+let myDoublyLinkedList = new MyDoublyLinkedList(1);
