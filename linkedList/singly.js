5b498d6821524f8da0922ada41d47d08739b35ad
Diego De Granda <degranda@gmail.com>
2020-10-21 10:15:27 -0500

    agregando insert

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -52,6 +52,33 @@ class MySinglyLinkedList {
 
     return this;
   }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+
+    return this;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
