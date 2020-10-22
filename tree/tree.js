4cf4d51ace8e6beaef9050e3d37c9f4997d8bd07
Diego De Granda <degranda@gmail.com>
2020-10-22 11:48:28 -0500

    agregando un tree con metodo insert

---
+++ tree/tree.js
@@ -0,0 +1,42 @@
+//      10
+//   4     20
+// 2  8  17  170
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
